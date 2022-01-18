b7a8bb40b6a3a6c6d3d17982da7f1aa858efcabe
Edwin Bautista <97236733+edwinbzg@users.noreply.github.com>
2022-01-18 14:15:46 -0500

    Update gcsfuse_run.sh

--- gcsfuse_run.sh
+++ gcsfuse_run.sh
@@ -20,7 +20,7 @@ set -eo pipefail
 # mkdir -p $MNT_DIR
 
 echo "Mounting GCS Fuse."
-gcsfuse --debug_gcs --debug_fuse $BUCKET $GEOSERVER_HOME 
+gcsfuse --debug_gcs --debug_fuse $BUCKET /opt/geoserver/ 
 echo "Mounting completed."
 
 # Run the web service on container startup. Here we use the gunicorn
